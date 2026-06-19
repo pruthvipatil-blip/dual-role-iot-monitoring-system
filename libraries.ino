cb4fd3759b0e98117b5318ee7038d35134fa1f67
pruthvipatil-blip <rp0485039@gmail.com>
2026-06-19 11:27:24 +0530

    Create libraries.ino with Wokwi library list
    Added a list of libraries used in the project.

---
+++ libraries.ino
@@ -0,0 +1,11 @@
+# Wokwi Library List
+# See https://docs.wokwi.com/guides/libraries
+
+# Automatically added based on includes:
+DHT sensor library
+
+Adafruit MQTT Library
+Adafruit Unified Sensor
+OneWire
+Adafruit GFX Library
+Adafruit SSD1306
